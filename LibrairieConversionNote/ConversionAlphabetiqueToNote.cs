b418b9ff70262d8bf8466b720202cb6a5d229fbb
JuniorZegarra <109171246+JuniorZegarra@users.noreply.github.com>
2022-07-19 17:12:42 -0400

    version 2

--- LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
+++ LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
@@ -1,7 +1,24 @@
 ﻿namespace LibrairieConversionNote
 {
-    public class Class1
+    public class ConversionAlphabetiqueToNote
     {
+        public string Convert(string alphabet)
+        {
+
+            switch (alphabet)
+            {
+                case "A": return "LA"; break;
+                case "B": return "SI"; break;
+                case "C": return "DO"; break;
+                case "D": return "RE"; break;
+                case "E": return "MI"; break;
+                case "F": return "FA"; break;
+                case "G": return "SOL"; break;
+                default: return string.Empty;
+
+            }
+
+        }
 
     }
 }
